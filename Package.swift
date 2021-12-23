29a866af9c389bede2c92c2cc8eec83e8c847b7b
Ali A. Hilal <ali94_200970@yahoo.com>
2021-12-23 22:53:00 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,32 +12,17 @@ let package = Package(
             name: "XCoordinator",
             type: .dynamic,
             targets: ["XCoordinator"]),
-        .library(
-            name: "XCoordinatorRx",
-            targets: ["XCoordinatorRx"]),
-        .library(
-            name: "XCoordinatorCombine",
-            targets: ["XCoordinatorCombine"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
+        // .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XCoordinator",
-            dependencies: []),
-        .target(
-            name: "XCoordinatorRx",
-            dependencies: ["XCoordinator", "RxSwift"]),
-        .target(
-            name: "XCoordinatorCombine",
-            dependencies: ["XCoordinator"]),
-        .testTarget(
-            name: "XCoordinatorTests",
-            dependencies: ["XCoordinator", "XCoordinatorRx"]),
+            dependencies: [])
     ]
 )
